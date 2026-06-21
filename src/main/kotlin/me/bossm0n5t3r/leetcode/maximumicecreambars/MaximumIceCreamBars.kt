0a0e635c0ab6116843dca3a757df273f44130d0c
bossm0n5t3r <bossm0n5t3r@gmail.com>
2026-06-21 22:10:00 +0900

    1833. Maximum Ice Cream Bars

--- src/main/kotlin/me/bossm0n5t3r/leetcode/maximumicecreambars/MaximumIceCreamBars.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/maximumicecreambars/MaximumIceCreamBars.kt
@@ -3,22 +3,19 @@ package me.bossm0n5t3r.leetcode.maximumicecreambars
 class MaximumIceCreamBars {
     class Solution {
         fun maxIceCream(costs: IntArray, coins: Int): Int {
-            val countIceCreams = IntArray(100_001) { 0 }
+            val countsByCost = IntArray(100_001)
             for (cost in costs) {
-                countIceCreams[cost]++
+                countsByCost[cost]++
             }
+
             var result = 0
-            var remainedCoins = coins
-            var cost = 1
-            while (remainedCoins > 0 && cost < 100_001) {
-                var count = countIceCreams[cost]
-                while (count > 0 && remainedCoins >= cost) {
-                    result++
-                    remainedCoins -= cost
-                    count--
-                }
-                if (remainedCoins < cost) break
-                cost++
+            var remainingCoins = coins
+
+            for (cost in 1 until countsByCost.size) {
+                if (remainingCoins < cost) break
+                val canBuy = minOf(countsByCost[cost], remainingCoins / cost)
+                result += canBuy
+                remainingCoins -= canBuy * cost
             }
             return result
         }
