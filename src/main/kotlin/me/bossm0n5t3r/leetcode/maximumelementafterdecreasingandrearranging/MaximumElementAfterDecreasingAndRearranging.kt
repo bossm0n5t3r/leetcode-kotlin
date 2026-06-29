433193f1180aff71e9944ebc6213d7438cd84e41
bossm0n5t3r <bossm0n5t3r@gmail.com>
2026-06-29 11:00:00 +0900

    1846. Maximum Element After Decreasing and Rearranging

--- src/main/kotlin/me/bossm0n5t3r/leetcode/maximumelementafterdecreasingandrearranging/MaximumElementAfterDecreasingAndRearranging.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/maximumelementafterdecreasingandrearranging/MaximumElementAfterDecreasingAndRearranging.kt
@@ -3,13 +3,16 @@ package me.bossm0n5t3r.leetcode.maximumelementafterdecreasingandrearranging
 class MaximumElementAfterDecreasingAndRearranging {
     class Solution {
         fun maximumElementAfterDecrementingAndRearranging(arr: IntArray): Int {
-            arr.sort()
-            arr[0] = 1
-            for (i in 1 until arr.size) {
-                if (arr[i] - arr[i - 1] <= 1) continue
-                arr[i] = arr[i - 1] + 1
+            val n = arr.size
+            val counts = IntArray(n + 1)
+            for (num in arr) {
+                counts[minOf(num, n)]++
             }
-            return arr.last()
+            var ans = 1
+            for (i in 2..n) {
+                ans = minOf(ans + counts[i], i)
+            }
+            return ans
         }
     }
 }
