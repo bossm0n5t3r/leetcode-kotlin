81a9b417e8772f2c44c3a03096fcd4e93de9cdc9
bossm0n5t3r <bossm0n5t3r@gmail.com>
2026-07-06 11:20:00 +0900

    1288. Remove Covered Intervals

--- src/main/kotlin/me/bossm0n5t3r/leetcode/removecoveredintervals/RemoveCoveredIntervals.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/removecoveredintervals/RemoveCoveredIntervals.kt
@@ -3,27 +3,25 @@ package me.bossm0n5t3r.leetcode.removecoveredintervals
 class RemoveCoveredIntervals {
     class Solution {
         fun removeCoveredIntervals(intervals: Array<IntArray>): Int {
-            val n = intervals.size
-            val sorted =
-                intervals.sortedWith(
-                    compareBy<IntArray> { it.first() }.thenByDescending { it.last() }
-                )
+            intervals.sortWith { a, b ->
+                if (a[0] != b[0]) a[0].compareTo(b[0]) else b[1].compareTo(a[1])
+            }
+
             var result = 0
             var index = 0
+            val n = intervals.size
             while (index < n) {
-                val cur = sorted[index++]
+                val curStart = intervals[index][0]
+                val curEnd = intervals[index][1]
                 result++
-                while (index < n && cur isCover sorted[index]) {
+                index++
+                while (
+                    index < n && curStart <= intervals[index][0] && intervals[index][1] <= curEnd
+                ) {
                     index++
                 }
             }
             return result
-        }
-
-        private infix fun IntArray.isCover(other: IntArray): Boolean {
-            val (a, b) = this
-            val (c, d) = other
-            return a <= c && d <= b
         }
     }
 }
