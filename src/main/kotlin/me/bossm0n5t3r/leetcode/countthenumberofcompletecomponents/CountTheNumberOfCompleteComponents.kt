35f7d1ceb2992b83dcc1e71ec5abc99445db470e
bossm0n5t3r <bossm0n5t3r@gmail.com>
2026-07-11 14:30:00 +0900

    2685. Count the Number of Complete Components

--- src/main/kotlin/me/bossm0n5t3r/leetcode/countthenumberofcompletecomponents/CountTheNumberOfCompleteComponents.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/countthenumberofcompletecomponents/CountTheNumberOfCompleteComponents.kt
@@ -3,60 +3,50 @@ package me.bossm0n5t3r.leetcode.countthenumberofcompletecomponents
 class CountTheNumberOfCompleteComponents {
     class Solution {
         fun countCompleteComponents(n: Int, edges: Array<IntArray>): Int {
-            val graph = Array(n) { mutableSetOf<Int>() }
+            val graph = Array(n) { mutableListOf<Int>() }
+
             for (edge in edges) {
-                val (u, v) = edge
-                graph[u] += v
-                graph[v] += u
+                val u = edge[0]
+                val v = edge[1]
+                graph[u].add(v)
+                graph[v].add(u)
             }
+
+            val visited = BooleanArray(n)
             var result = 0
-            val visited = BooleanArray(n) { false }
-            var cur = 0
-            while (cur < n) {
-                if (visited[cur]) {
-                    cur++
-                    continue
+
+            fun dfs(node: Int, component: MutableList<Int>) {
+                visited[node] = true
+                component.add(node)
+
+                for (next in graph[node]) {
+                    if (!visited[next]) {
+                        dfs(next, component)
+                    }
                 }
-                if (isCompleteComponents(graph, cur, visited)) {
-                    result++
+            }
+
+            for (i in 0 until n) {
+                if (!visited[i]) {
+                    val component = mutableListOf<Int>()
+                    dfs(i, component)
+
+                    val nodes = component.size
+                    var degreeSum = 0
+
+                    for (node in component) {
+                        degreeSum += graph[node].size
+                    }
+
+                    val actualEdges = degreeSum / 2
+                    val requiredEdges = nodes * (nodes - 1) / 2
+
+                    if (actualEdges == requiredEdges) {
+                        result++
+                    }
                 }
-                cur++
             }
             return result
-        }
-
-        private fun isCompleteComponents(
-            graph: Array<MutableSet<Int>>,
-            cur: Int,
-            visited: BooleanArray,
-        ): Boolean {
-            val component = mutableSetOf<Int>()
-            dfs(cur, visited, graph, component)
-            val size = component.size
-            val expectedEdges = size * (size - 1) / 2
-            var actualEdges = 0
-            for (u in component) {
-                for (v in graph[u]) {
-                    if (v in component) actualEdges++
-                }
-            }
-            actualEdges /= 2
-            return actualEdges == expectedEdges
-        }
-
-        private fun dfs(
-            node: Int,
-            visited: BooleanArray,
-            graph: Array<MutableSet<Int>>,
-            component: MutableSet<Int>,
-        ) {
-            visited[node] = true
-            component += node
-            for (neighbor in graph[node]) {
-                if (!visited[neighbor]) {
-                    dfs(neighbor, visited, graph, component)
-                }
-            }
         }
     }
 }
