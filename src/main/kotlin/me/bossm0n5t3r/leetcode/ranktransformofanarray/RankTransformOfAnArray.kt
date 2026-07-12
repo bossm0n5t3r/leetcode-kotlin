8c9205b52cd1fa3721cd0da7fcf53cb91cd926c7
bossm0n5t3r <bossm0n5t3r@gmail.com>
2026-07-12 23:05:00 +0900

    1331. Rank Transform of an Array

--- src/main/kotlin/me/bossm0n5t3r/leetcode/ranktransformofanarray/RankTransformOfAnArray.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/ranktransformofanarray/RankTransformOfAnArray.kt
@@ -4,12 +4,7 @@ class RankTransformOfAnArray {
     class Solution {
         fun arrayRankTransform(arr: IntArray): IntArray {
             val numToRank =
-                arr.toList()
-                    .groupBy { it }
-                    .keys
-                    .sorted()
-                    .mapIndexed { index, i -> i to index + 1 }
-                    .toMap()
+                arr.distinct().sorted().mapIndexed { index, i -> i to index + 1 }.toMap()
             return arr.toList().mapNotNull { numToRank[it] }.toIntArray()
         }
     }
