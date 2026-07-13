b6c2906b453c62c42340bc8babac7e46db57bb6b
bossm0n5t3r <bossm0n5t3r@gmail.com>
2026-07-13 11:15:00 +0900

    1291. Sequential Digits

--- src/main/kotlin/me/bossm0n5t3r/leetcode/sequentialDigits/SequentialDigits.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/sequentialDigits/SequentialDigits.kt
@@ -2,12 +2,23 @@ package me.bossm0n5t3r.leetcode.sequentialDigits
 
 class SequentialDigits {
     class Solution {
-        fun sequentialDigits(low: Int, high: Int): List<Int> =
-            (low.toString().length..high.toString().length).flatMap { n ->
-                generateSequentialDigits(n, low, high)
-            }
+        fun sequentialDigits(low: Int, high: Int): List<Int> {
+            val result = mutableListOf<Int>()
+            val minLen = low.toString().length
+            val maxLen = high.toString().length
 
-        private fun generateSequentialDigits(n: Int, low: Int, high: Int): List<Int> =
-            (1..9).windowed(n).map { it.joinToString("").toInt() }.filter { it in low..high }
+            for (length in minLen..maxLen) {
+                for (start in 1..(10 - length)) {
+                    var num = 0
+                    for (digit in start until start + length) {
+                        num = num * 10 + digit
+                    }
+                    if (num in low..high) {
+                        result.add(num)
+                    }
+                }
+            }
+            return result
+        }
     }
 }
