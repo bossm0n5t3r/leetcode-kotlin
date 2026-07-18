24a548c88ae77c09468166761ec24c753d9dd406
bossm0n5t3r <bossm0n5t3r@gmail.com>
2026-07-18 13:25:00 +0900

    1979. Find Greatest Common Divisor of Array

--- src/main/kotlin/me/bossm0n5t3r/leetcode/findgreatestcommondivisorofarray/FindGreatestCommonDivisorOfArray.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/findgreatestcommondivisorofarray/FindGreatestCommonDivisorOfArray.kt
@@ -3,19 +3,15 @@ package me.bossm0n5t3r.leetcode.findgreatestcommondivisorofarray
 class FindGreatestCommonDivisorOfArray {
     class Solution {
         fun findGCD(nums: IntArray): Int {
-            nums.sort()
-            return gcd(nums.first(), nums.last())
+            var min = nums[0]
+            var max = nums[0]
+            for (num in nums) {
+                if (num < min) min = num
+                if (num > max) max = num
+            }
+            return gcd(min, max)
         }
 
-        private fun gcd(a: Int, b: Int): Int {
-            var x = a
-            var y = b
-            while (y != 0) {
-                val tmp = x
-                x = y
-                y = tmp % y
-            }
-            return x
-        }
+        private tailrec fun gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
     }
 }
