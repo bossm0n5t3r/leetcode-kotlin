b953ea020057d68474f22f58e1fdbe402523fc99
bossm0n5t3r <bossm0n5t3r@gmail.com>
2026-07-30 15:15:00 +0900

    3014. Minimum Number of Pushes to Type Word I

--- src/main/kotlin/me/bossm0n5t3r/leetcode/minimumnumberofpushestotypewordi/MinimumNumberOfPushesToTypeWordI.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/minimumnumberofpushestotypewordi/MinimumNumberOfPushesToTypeWordI.kt
@@ -1,29 +1,17 @@
 package me.bossm0n5t3r.leetcode.minimumnumberofpushestotypewordi
 
-import java.util.PriorityQueue
-
 class MinimumNumberOfPushesToTypeWordI {
     class Solution {
         fun minimumPushes(word: String): Int {
-            val frequency = mutableMapOf<Char, Int>()
-            for (char in word) {
-                frequency[char] = frequency.getOrDefault(char, 0) + 1
-            }
-            val pq = PriorityQueue<Int>()
-            pq.addAll(frequency.values)
-            var result = 0
-            var digits = 8
-            var push = 1
-            while (pq.isNotEmpty()) {
-                val cur = pq.poll()
-                result += cur * push
-                digits--
-                if (digits == 0) {
-                    digits = 8
-                    push++
-                }
-            }
-            return result
+            val fullKeyGroups = word.length / KEYS_PER_PUSH_COUNT
+            val remainingCharacters = word.length % KEYS_PER_PUSH_COUNT
+
+            return KEYS_PER_PUSH_COUNT * fullKeyGroups * (fullKeyGroups + 1) / 2 +
+                remainingCharacters * (fullKeyGroups + 1)
+        }
+
+        private companion object {
+            const val KEYS_PER_PUSH_COUNT = 8
         }
     }
 }
--- src/test/kotlin/me/bossm0n5t3r/leetcode/minimumnumberofpushestotypewordi/MinimumNumberOfPushesToTypeWordITest.kt
+++ src/test/kotlin/me/bossm0n5t3r/leetcode/minimumnumberofpushestotypewordi/MinimumNumberOfPushesToTypeWordITest.kt
@@ -10,7 +10,16 @@ class MinimumNumberOfPushesToTypeWordITest {
 
     @Test
     fun test() {
-        val testDataList = listOf(TestData("abcde", 5), TestData("xycdefghij", 12))
+        val testDataList =
+            listOf(
+                TestData("abcde", 5),
+                TestData("abcdefgh", 8),
+                TestData("abcdefghi", 10),
+                TestData("xycdefghij", 12),
+                TestData("abcdefghijklmnop", 24),
+                TestData("abcdefghijklmnopq", 27),
+                TestData("abcdefghijklmnopqrstuvwxyz", 56),
+            )
 
         for (testData in testDataList) {
             assertEquals(testData.result, sut.minimumPushes(testData.word))
