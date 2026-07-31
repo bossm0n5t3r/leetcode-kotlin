2b935f65b74f660c08ad86412d0387bc23fb88e6
bossm0n5t3r <bossm0n5t3r@gmail.com>
2026-07-31 14:45:00 +0900

    3016. Minimum Number of Pushes to Type Word II

--- src/main/kotlin/me/bossm0n5t3r/leetcode/minimumnumberofpushestotypewordii/MinimumNumberOfPushesToTypeWordII.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/minimumnumberofpushestotypewordii/MinimumNumberOfPushesToTypeWordII.kt
@@ -1,27 +1,18 @@
 package me.bossm0n5t3r.leetcode.minimumnumberofpushestotypewordii
 
-import java.util.PriorityQueue
-
 class MinimumNumberOfPushesToTypeWordII {
     class Solution {
         fun minimumPushes(word: String): Int {
-            val frequency = mutableMapOf<Char, Int>()
+            val frequencies = IntArray(26)
             for (char in word) {
-                frequency[char] = frequency.getOrDefault(char, 0) + 1
+                frequencies[char - 'a']++
             }
-            val pq = PriorityQueue<Int>(compareByDescending { it })
-            pq.addAll(frequency.values)
+
+            frequencies.sortDescending()
+
             var result = 0
-            var digits = 8
-            var push = 1
-            while (pq.isNotEmpty()) {
-                val cur = pq.poll()
-                result += cur * push
-                digits--
-                if (digits == 0) {
-                    digits = 8
-                    push++
-                }
+            for (index in frequencies.indices) {
+                result += frequencies[index] * (index / 8 + 1)
             }
             return result
         }
