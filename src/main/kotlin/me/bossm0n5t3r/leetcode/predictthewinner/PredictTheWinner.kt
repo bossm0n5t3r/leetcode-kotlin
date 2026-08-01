8833f4a68bf76c53a4ef8eadb7d88f5ddc008ab0
bossm0n5t3r <bossm0n5t3r@gmail.com>
2026-08-01 14:10:00 +0900

    486. Predict the Winner

--- src/main/kotlin/me/bossm0n5t3r/leetcode/predictthewinner/PredictTheWinner.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/predictthewinner/PredictTheWinner.kt
@@ -4,22 +4,20 @@ class PredictTheWinner {
     class Solution {
         fun predictTheWinner(nums: IntArray): Boolean {
             val n = nums.size
-            val dp = Array(n) { IntArray(n) }
+            val memo = Array(n) { IntArray(n) { Int.MIN_VALUE } }
 
-            for (i in 0 until n) {
-                dp[i][i] = nums[i]
+            fun scoreDiff(i: Int, j: Int): Int {
+                if (i == j) return nums[i]
+                if (memo[i][j] != Int.MIN_VALUE) return memo[i][j]
+
+                val takeLeft = nums[i] - scoreDiff(i + 1, j)
+                val takeRight = nums[j] - scoreDiff(i, j - 1)
+
+                memo[i][j] = maxOf(takeLeft, takeRight)
+                return memo[i][j]
             }
 
-            for (len in 2..n) {
-                for (i in 0..n - len) {
-                    val j = i + len - 1
-                    val takeLeft = nums[i] - dp[i + 1][j]
-                    val takeRight = nums[j] - dp[i][j - 1]
-                    dp[i][j] = maxOf(takeLeft, takeRight)
-                }
-            }
-
-            return dp[0][n - 1] >= 0
+            return scoreDiff(0, n - 1) >= 0
         }
     }
 }
