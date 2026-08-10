877bceb452bfd2c9c2edb095c1f2b77df7533f75
bossm0n5t3r <bossm0n5t3r@gmail.com>
2026-08-10 20:40:00 +0900

    1510. Stone Game IV

--- src/main/kotlin/me/bossm0n5t3r/leetcode/stonegameiv/StoneGameIV.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/stonegameiv/StoneGameIV.kt
@@ -3,26 +3,18 @@ package me.bossm0n5t3r.leetcode.stonegameiv
 class StoneGameIV {
     class Solution {
         fun winnerSquareGame(n: Int): Boolean {
-            val memo = mutableMapOf(0 to false)
-
-            fun canWin(remaining: Int): Boolean {
-                val cached = memo[remaining]
-                if (cached != null) return cached
-
-                var times = 1
-                while (times * times <= remaining) {
-                    if (!canWin(remaining - times * times)) {
-                        memo[remaining] = true
-                        return true
+            val dp = BooleanArray(n + 1)
+            for (i in 1..n) {
+                var j = 1
+                while (j * j <= i) {
+                    if (!dp[i - j * j]) {
+                        dp[i] = true
+                        break
                     }
-                    times++
+                    j++
                 }
-
-                memo[remaining] = false
-                return false
             }
-
-            return canWin(n)
+            return dp[n]
         }
     }
 }
