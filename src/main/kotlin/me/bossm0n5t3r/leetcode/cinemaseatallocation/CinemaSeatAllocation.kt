bf02ccc33774d31a07932c954a8eff7a5723a001
bossm0n5t3r <bossm0n5t3r@gmail.com>
2026-08-19 10:50:00 +0900

    1386. Cinema Seat Allocation

--- src/main/kotlin/me/bossm0n5t3r/leetcode/cinemaseatallocation/CinemaSeatAllocation.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/cinemaseatallocation/CinemaSeatAllocation.kt
@@ -3,45 +3,27 @@ package me.bossm0n5t3r.leetcode.cinemaseatallocation
 class CinemaSeatAllocation {
     class Solution {
         fun maxNumberOfFamilies(n: Int, reservedSeats: Array<IntArray>): Int {
-            val sortedReservedSeats =
-                reservedSeats.sortedWith(
-                    compareBy<IntArray> { it.first() }.thenComparing { it.last() }
-                )
-            var result = n * 2
-            var curRow = 1
-            val availableSeats = BooleanArray(3) { true }
-            for ((row, seat) in sortedReservedSeats) {
-                if (curRow != row) {
-                    result -=
-                        when {
-                            availableSeats[0] && availableSeats[2] -> 0
-                            availableSeats.none { it } -> 2
-                            else -> 1
-                        }
-                    curRow = row
-                    availableSeats[0] = true
-                    availableSeats[1] = true
-                    availableSeats[2] = true
-                }
-                when (seat) {
-                    in 2..3 -> availableSeats[0] = false
-                    in 4..5 -> {
-                        availableSeats[0] = false
-                        availableSeats[1] = false
-                    }
-                    in 6..7 -> {
-                        availableSeats[1] = false
-                        availableSeats[2] = false
-                    }
-                    in 8..9 -> availableSeats[2] = false
+            val left = 0b11110000
+            val middle = 0b11000011
+            val right = 0b00001111
+            val occupied = mutableMapOf<Int, Int>()
+            for ((row, seat) in reservedSeats) {
+                if (seat in 2..9) {
+                    val origin: Int = occupied[row] ?: 0
+                    val value = origin or (1 shl (seat - 2))
+                    occupied[row] = value
                 }
             }
-            result -=
-                when {
-                    availableSeats[0] && availableSeats[2] -> 0
-                    availableSeats.none { it } -> 2
-                    else -> 1
+            var result = (n - occupied.size) * 2
+            for ((_, bitmask) in occupied) {
+                if (
+                    (bitmask or left) == left ||
+                        (bitmask or middle) == middle ||
+                        (bitmask or right) == right
+                ) {
+                    ++result
                 }
+            }
             return result
         }
     }
