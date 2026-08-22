b4e4d81c432ec0aa6c84f6467101a4c0ce3ebe26
bossm0n5t3r <bossm0n5t3r@gmail.com>
2026-08-23 00:35:00 +0900

    20. Valid Parentheses

--- src/main/kotlin/me/bossm0n5t3r/leetcode/validParentheses/ValidParentheses.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/validParentheses/ValidParentheses.kt
@@ -5,8 +5,8 @@ class ValidParentheses {
         fun isValid(s: String): Boolean {
             val map = mapOf(')' to '(', '}' to '{', ']' to '[')
             val stack = mutableListOf<Char>()
-            s.forEach { c ->
-                if (c in map.keys) {
+            for (c in s) {
+                if (c in map) {
                     if (stack.isEmpty() || stack.last() != map[c]) return false
                     stack.removeAt(stack.size - 1)
                 } else {
