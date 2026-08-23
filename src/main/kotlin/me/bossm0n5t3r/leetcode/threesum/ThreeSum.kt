03ca0a6b0edae72909cdea527b0caf4cb670083f
bossm0n5t3r <bossm0n5t3r@gmail.com>
2026-08-24 08:45:00 +0900

    15. 3Sum

---
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/threesum/ThreeSum.kt
@@ -0,0 +1,32 @@
+package me.bossm0n5t3r.leetcode.threesum
+
+class ThreeSum {
+    class Solution {
+        fun threeSum(nums: IntArray): List<List<Int>> {
+            nums.sort()
+            val result = mutableListOf<List<Int>>()
+            for (i in 0 until nums.size - 2) {
+                if (i > 0 && nums[i] == nums[i - 1]) {
+                    continue
+                }
+                var left = i + 1
+                var right = nums.lastIndex
+                while (left < right) {
+                    val sum = nums[i] + nums[left] + nums[right]
+                    when {
+                        sum == 0 -> {
+                            result.add(listOf(nums[i], nums[left], nums[right]))
+                            while (left < right && nums[left] == nums[left + 1]) left++
+                            while (left < right && nums[right] == nums[right - 1]) right--
+                            left++
+                            right--
+                        }
+                        sum < 0 -> left++
+                        else -> right--
+                    }
+                }
+            }
+            return result
+        }
+    }
+}
--- src/test/kotlin/me/bossm0n5t3r/leetcode/threesum/ThreeSumTest.kt
+++ src/test/kotlin/me/bossm0n5t3r/leetcode/threesum/ThreeSumTest.kt
@@ -1,4 +1,4 @@
-package me.bossm0n5t3r.leetcode.threeSum
+package me.bossm0n5t3r.leetcode.threesum
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
