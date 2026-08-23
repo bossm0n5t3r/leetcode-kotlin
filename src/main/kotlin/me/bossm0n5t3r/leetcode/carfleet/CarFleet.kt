cb14074e0c7da2915cd4425e62ca0bae8e93df43
bossm0n5t3r <bossm0n5t3r@gmail.com>
2026-08-24 07:40:00 +0900

    853. Car Fleet

--- src/main/kotlin/me/bossm0n5t3r/leetcode/carfleet/CarFleet.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/carfleet/CarFleet.kt
@@ -4,19 +4,18 @@ class CarFleet {
     class Solution {
         fun carFleet(target: Int, position: IntArray, speed: IntArray): Int {
             val n = position.size
-            val arrivalTimes =
-                position
-                    .zip(speed)
-                    .sortedBy { it.first }
-                    .map { (p, s) -> (target - p).toDouble() / s }
+            val arrivalTimes = DoubleArray(target)
+            for (i in 0 until n) {
+                arrivalTimes[position[i]] = (target - position[i]).toDouble() / speed[i]
+            }
             var result = 0
-            var index = n - 1
-            while (index >= 0) {
-                val cur = arrivalTimes[index--]
-                while (index >= 0 && arrivalTimes[index] <= cur) {
-                    index--
+            var prev = 0.0
+            for (i in target - 1 downTo 0) {
+                val cur = arrivalTimes[i]
+                if (cur > prev) {
+                    result++
+                    prev = cur
                 }
-                result++
             }
             return result
         }
