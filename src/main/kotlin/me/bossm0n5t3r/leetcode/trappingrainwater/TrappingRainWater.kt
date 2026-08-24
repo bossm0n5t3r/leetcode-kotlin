d18366df85d82f1bfdec980470fd009fbb85fdf1
bossm0n5t3r <bossm0n5t3r@gmail.com>
2026-08-24 09:30:00 +0900

    42. Trapping Rain Water

--- src/main/kotlin/me/bossm0n5t3r/leetcode/trappingrainwater/TrappingRainWater.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/trappingrainwater/TrappingRainWater.kt
@@ -3,13 +3,10 @@ package me.bossm0n5t3r.leetcode.trappingrainwater
 class TrappingRainWater {
     class Solution {
         fun trap(height: IntArray): Int {
-            val n = height.size
-            var leftIndex = 0
-            var rightIndex = n - 1
+            var (leftIndex, rightIndex) = 0 to height.lastIndex
             var leftMax = height[leftIndex]
             var rightMax = height[rightIndex]
             var water = 0
-
             while (leftIndex < rightIndex) {
                 if (leftMax < rightMax) {
                     leftIndex++
@@ -21,7 +18,6 @@ class TrappingRainWater {
                     water += rightMax - height[rightIndex]
                 }
             }
-
             return water
         }
     }
