f94c70db5334fcb4e7a5c51fcee5ffcd66a2cbdb
bossm0n5t3r <bossm0n5t3r@gmail.com>
2026-08-27 05:30:00 +0900

    76. Minimum Window Substring

--- src/main/kotlin/me/bossm0n5t3r/leetcode/minimumwindowsubstring/MinimumWindowSubstring.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/minimumwindowsubstring/MinimumWindowSubstring.kt
@@ -2,37 +2,49 @@ package me.bossm0n5t3r.leetcode.minimumwindowsubstring
 
 class MinimumWindowSubstring {
     class Solution {
-        private val lowercase = 'a'..'z'
-        private val uppercase = 'A'..'Z'
-
         fun minWindow(s: String, t: String): String {
-            val tFrequency = IntArray(123) { 0 }
+            val tFrequency = IntArray(123)
             for (c in t) tFrequency[c.code]++
-            var left = 0
-            var result = ""
-            val sFrequency = IntArray(123) { 0 }
-            for (right in s.indices) {
-                val cur = s[right]
-                sFrequency[cur.code]++
-                while (left < s.length && sFrequency.isContained(tFrequency)) {
-                    val candidate = s.substring(left, right + 1)
-                    if (result.isEmpty() || candidate.length < result.length) result = candidate
 
-                    sFrequency[s[left].code]--
+            val required = tFrequency.count { it > 0 }
+            val sFrequency = IntArray(123)
+
+            var left = 0
+            var formed = 0
+
+            var resultStart = 0
+            var resultLength = Int.MAX_VALUE
+
+            for (right in s.indices) {
+                val rc = s[right]
+                sFrequency[rc.code]++
+                if (tFrequency[rc.code] > 0 && sFrequency[rc.code] == tFrequency[rc.code]) {
+                    formed++
+                }
+                while (formed == required) {
+                    val windowLength = right - left + 1
+
+                    if (windowLength < resultLength) {
+                        resultStart = left
+                        resultLength = windowLength
+                    }
+
+                    val lc = s[left]
+
+                    if (tFrequency[lc.code] > 0 && sFrequency[lc.code] == tFrequency[lc.code]) {
+                        formed--
+                    }
+
+                    sFrequency[lc.code]--
                     left++
                 }
             }
-            return result
-        }
 
-        private fun IntArray.isContained(other: IntArray): Boolean {
-            for (l in lowercase) {
-                if (this[l.code] < other[l.code]) return false
+            return if (resultLength == Int.MAX_VALUE) {
+                ""
+            } else {
+                s.substring(resultStart, resultStart + resultLength)
             }
-            for (l in uppercase) {
-                if (this[l.code] < other[l.code]) return false
-            }
-            return true
         }
     }
 }
