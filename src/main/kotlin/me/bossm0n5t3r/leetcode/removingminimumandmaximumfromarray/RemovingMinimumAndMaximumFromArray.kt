3f3884f49cf21dd98b39baafe055fd54f3b2cc6c
bossm0n5t3r <bossm0n5t3r@gmail.com>
2026-08-30 20:15:00 +0900

    2091. Removing Minimum and Maximum From Array

--- src/main/kotlin/me/bossm0n5t3r/leetcode/removingminimumandmaximumfromarray/RemovingMinimumAndMaximumFromArray.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/removingminimumandmaximumfromarray/RemovingMinimumAndMaximumFromArray.kt
@@ -4,36 +4,19 @@ class RemovingMinimumAndMaximumFromArray {
     class Solution {
         fun minimumDeletions(nums: IntArray): Int {
             val n = nums.size
-            if (n <= 2) return n
 
-            var minValue = Int.MAX_VALUE
-            var maxValue = Int.MIN_VALUE
-
-            var minIndex = -1
-            var maxIndex = -1
+            var minIndex = 0
+            var maxIndex = 0
 
             for (i in nums.indices) {
-                val num = nums[i]
-                if (num < minValue) {
-                    minValue = num
-                    minIndex = i
-                }
-                if (maxValue < num) {
-                    maxValue = num
-                    maxIndex = i
-                }
+                if (nums[i] < nums[minIndex]) minIndex = i
+                if (nums[i] > nums[maxIndex]) maxIndex = i
             }
 
-            val deleteFrontMin = minIndex + 1
-            val deleteFrontMax = maxIndex + 1
-            val deleteBackMin = n - minIndex
-            val deleteBackMax = n - maxIndex
+            val left = minOf(minIndex, maxIndex)
+            val right = maxOf(minIndex, maxIndex)
 
-            return minOf(
-                minOf(deleteFrontMin, deleteFrontMax) + minOf(deleteBackMin, deleteBackMax),
-                maxOf(deleteFrontMin, deleteFrontMax),
-                maxOf(deleteBackMin, deleteBackMax),
-            )
+            return minOf(right + 1, n - left, left + 1 + n - right)
         }
     }
 }
