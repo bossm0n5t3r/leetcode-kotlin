377ed91e6e13156a43a582340a5beaaddb1c46c1
bossm0n5t3r <bossm0n5t3r@gmail.com>
2026-08-31 16:00:00 +0900

    213. House Robber II

--- src/main/kotlin/me/bossm0n5t3r/leetcode/houserobberii/HouseRobberII.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/houserobberii/HouseRobberII.kt
@@ -3,21 +3,21 @@ package me.bossm0n5t3r.leetcode.houserobberii
 class HouseRobberII {
     class Solution {
         fun rob(nums: IntArray): Int {
-            val n = nums.size
-            if (n == 1) return nums[0]
-            if (n == 2) return maxOf(nums[0], nums[1])
-            return maxOf(doDp(nums.drop(1)), doDp(nums.dropLast(1)))
+            if (nums.size == 1) return nums[0]
+            return maxOf(rob(nums, 0, nums.lastIndex - 1), rob(nums, 1, nums.lastIndex))
         }
 
-        private fun doDp(nums: List<Int>): Int {
-            val n = nums.size
-            val dp = IntArray(n)
-            dp[0] = nums[0]
-            dp[1] = maxOf(dp[0], nums[1])
-            for (i in 2 until n) {
-                dp[i] = maxOf(dp[i - 2] + nums[i], dp[i - 1])
+        private fun rob(nums: IntArray, start: Int, end: Int): Int {
+            var prev2 = 0
+            var prev1 = 0
+
+            for (i in start..end) {
+                val current = maxOf(prev1, prev2 + nums[i])
+                prev2 = prev1
+                prev1 = current
             }
-            return maxOf(dp[nums.size - 1], dp[nums.size - 2])
+
+            return prev1
         }
     }
 }
