b914ba0aab1dcc25702ee4e0de75e1a6b73a18a8
bossm0n5t3r <bossm0n5t3r@gmail.com>
2026-09-04 09:45:00 +0900

    3903. Smallest Stable Index I

--- src/main/kotlin/me/bossm0n5t3r/leetcode/smalleststableindexi/SmallestStableIndexI.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/smalleststableindexi/SmallestStableIndexI.kt
@@ -4,18 +4,17 @@ class SmallestStableIndexI {
     class Solution {
         fun firstStableIndex(nums: IntArray, k: Int): Int {
             val n = nums.size
-            val max = IntArray(n) { Int.MIN_VALUE }
-            max[0] = nums[0]
-            for (i in 1 until n) {
-                max[i] = if (nums[i] > max[i - 1]) nums[i] else max[i - 1]
-            }
-            val min = IntArray(n) { Int.MAX_VALUE }
-            min[n - 1] = nums[n - 1]
+            val suffixMin = IntArray(n) { Int.MAX_VALUE }
+            suffixMin[n - 1] = nums[n - 1]
             for (i in n - 2 downTo 0) {
-                min[i] = if (nums[i] < min[i + 1]) nums[i] else min[i + 1]
+                suffixMin[i] = if (nums[i] < suffixMin[i + 1]) nums[i] else suffixMin[i + 1]
             }
+            var prefixMax = nums[0]
             for (i in 0 until n) {
-                if (max[i] - min[i] <= k) return i
+                prefixMax = maxOf(prefixMax, nums[i])
+                if (prefixMax - suffixMin[i] <= k) {
+                    return i
+                }
             }
             return -1
         }
