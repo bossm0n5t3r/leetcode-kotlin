63ceb1ac42d75bdf16c52a472aa49da85c92979a
bossm0n5t3r <bossm0n5t3r@gmail.com>
2026-09-05 11:33:00 +0900

    3904. Smallest Stable Index II

---
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/smalleststableindexii/README.md
@@ -0,0 +1,3 @@
+# 3904. Smallest Stable Index II
+
+- [https://leetcode.com/problems/smallest-stable-index-ii/](https://leetcode.com/problems/smallest-stable-index-ii/)
---
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/smalleststableindexii/SmallestStableIndexII.kt
@@ -0,0 +1,22 @@
+package me.bossm0n5t3r.leetcode.smalleststableindexii
+
+class SmallestStableIndexII {
+    class Solution {
+        fun firstStableIndex(nums: IntArray, k: Int): Int {
+            val n = nums.size
+            val suffixMin = IntArray(n) { Int.MAX_VALUE }
+            suffixMin[n - 1] = nums[n - 1]
+            for (i in n - 2 downTo 0) {
+                suffixMin[i] = if (nums[i] < suffixMin[i + 1]) nums[i] else suffixMin[i + 1]
+            }
+            var prefixMax = nums[0]
+            for (i in 0 until n) {
+                prefixMax = maxOf(prefixMax, nums[i])
+                if (prefixMax - suffixMin[i] <= k) {
+                    return i
+                }
+            }
+            return -1
+        }
+    }
+}
