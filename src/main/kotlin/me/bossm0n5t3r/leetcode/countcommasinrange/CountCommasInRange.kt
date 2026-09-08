6a76cf4c66c449a5051bd89b5630790c782904a8
bossm0n5t3r <bossm0n5t3r@gmail.com>
2026-09-08 14:30:00 +0900

    3870. Count Commas in Range

--- src/main/kotlin/me/bossm0n5t3r/leetcode/countcommasinrange/CountCommasInRange.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/countcommasinrange/CountCommasInRange.kt
@@ -3,20 +3,13 @@ package me.bossm0n5t3r.leetcode.countcommasinrange
 class CountCommasInRange {
     class Solution {
         fun countCommas(n: Int): Int {
-            if (n < 1000) return 0
-            val thousands = ArrayDeque<Int>()
-            var tmp = n
-            while (tmp > 0) {
-                thousands.addFirst(tmp % 1000)
-                tmp /= 1000
+            var result = 0L
+            var threshold = 1_000L
+            while (threshold <= n) {
+                result += n.toLong() - threshold + 1
+                threshold *= 1_000
             }
-            var result = thousands.removeLastOrNull()?.let { it + 1 } ?: return 0
-            var times = 1000
-            for (i in thousands.lastIndex downTo 0) {
-                result += (thousands[i] - 1) * times
-                times += 1000
-            }
-            return result
+            return result.toInt()
         }
     }
 }
