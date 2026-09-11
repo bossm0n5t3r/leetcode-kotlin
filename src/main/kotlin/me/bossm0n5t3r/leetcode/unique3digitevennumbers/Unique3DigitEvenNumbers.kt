9712d03e6398e71719ce5a5f1736137cc0858578
bossm0n5t3r <bossm0n5t3r@gmail.com>
2026-09-11 14:10:00 +0900

    3483. Unique 3-Digit Even Numbers

--- src/main/kotlin/me/bossm0n5t3r/leetcode/unique3digitevennumbers/Unique3DigitEvenNumbers.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/unique3digitevennumbers/Unique3DigitEvenNumbers.kt
@@ -3,30 +3,27 @@ package me.bossm0n5t3r.leetcode.unique3digitevennumbers
 class Unique3DigitEvenNumbers {
     class Solution {
         fun totalNumbers(digits: IntArray): Int {
-            val result = mutableSetOf<String>()
-            val visited = BooleanArray(digits.size)
-            dfs(digits, visited, "", result)
-            return result.size
-        }
-
-        private fun dfs(
-            digits: IntArray,
-            visited: BooleanArray,
-            cur: String,
-            result: MutableSet<String>,
-        ) {
-            if (cur.length == 3) {
-                if (cur[0] != '0' && cur[2].digitToInt() % 2 == 0) {
-                    result += cur
+            val frequency = IntArray(10)
+            for (digit in digits) frequency[digit]++
+            var result = 0
+            for (number in 100..998 step 2) {
+                val hundreds = number / 100
+                val tens = (number / 10) % 10
+                val ones = number % 10
+                val required = IntArray(10)
+                required[hundreds]++
+                required[tens]++
+                required[ones]++
+                var canMake = true
+                for (digit in 0..9) {
+                    if (required[digit] > frequency[digit]) {
+                        canMake = false
+                        break
+                    }
                 }
-                return
+                if (canMake) result++
             }
-            for (i in digits.indices) {
-                if (visited[i]) continue
-                visited[i] = true
-                dfs(digits, visited, cur + digits[i], result)
-                visited[i] = false
-            }
+            return result
         }
     }
 }
