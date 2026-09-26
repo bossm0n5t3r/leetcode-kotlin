0c136d4ab8292cd692f60ae8cccd38e20e5b445b
bossm0n5t3r <bossm0n5t3r@gmail.com>
2026-09-26 13:35:00 +0900

    1807. Evaluate the Bracket Pairs of a String

--- src/main/kotlin/me/bossm0n5t3r/leetcode/evaluatethebracketpairsofastring/EvaluateTheBracketPairsOfAString.kt
+++ src/main/kotlin/me/bossm0n5t3r/leetcode/evaluatethebracketpairsofastring/EvaluateTheBracketPairsOfAString.kt
@@ -4,22 +4,22 @@ class EvaluateTheBracketPairsOfAString {
     class Solution {
         fun evaluate(s: String, knowledge: List<List<String>>): String {
             val result = StringBuilder()
-            val queue = StringBuilder()
-            var isBracket = false
+            val keyBuilder = StringBuilder()
+            var inBracket = false
             val knowledgeMap = knowledge.associate { it.first() to it.last() }
             for (c in s) {
                 when (c) {
-                    '(' -> isBracket = true
+                    '(' -> inBracket = true
                     ')' -> {
-                        isBracket = false
-                        val key = queue.toString()
+                        val key = keyBuilder.toString()
                         val value = knowledgeMap[key] ?: "?"
                         result.append(value)
-                        queue.clear()
+                        inBracket = false
+                        keyBuilder.clear()
                     }
                     else -> {
-                        if (isBracket) {
-                            queue.append(c)
+                        if (inBracket) {
+                            keyBuilder.append(c)
                         } else {
                             result.append(c)
                         }
